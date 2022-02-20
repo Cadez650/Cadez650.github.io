c7f8514bbfa3a21749eb84dd0e52f850b297e51d
Cadez650 <67844200+Cadez650@users.noreply.github.com>
2022-02-20 01:18:26 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ function json(url) {
   return fetch(url).then(res => 
     {
       res.json()
-                                
-                                });
+     });
 }
 
 let apiKey = '5b5f14723ccf3f576ce8c2e95b3b68bf9151303ea50c447f84ba9371';
-json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
+fetch(`https://api.ipdata.co?api-key=${apiKey}`).then(res => {res.json}).then(data => {
   console.log(data.ip);
   console.log(data.city);
   console.log(data.country_code);
