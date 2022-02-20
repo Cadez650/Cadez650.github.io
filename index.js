7c60a8154a7e5c0936ed077bee06b4a7a7272372
Cadez650 <67844200+Cadez650@users.noreply.github.com>
2022-02-20 01:05:08 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+function json(url) {
+  return fetch(url).then(res => res.json());
+}
+
+let apiKey = 'your_api_key';
+json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
+  console.log(data.ip);
+  console.log(data.city);
+  console.log(data.country_code);
+  // so many more properties
+});
