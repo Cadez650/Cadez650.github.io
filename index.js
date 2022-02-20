63999d806eb97c69d379886103645ffde6d9cf4f
Cadez650 <67844200+Cadez650@users.noreply.github.com>
2022-02-20 01:07:25 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function json(url) {
   return fetch(url).then(res => res.json());
 }
 
-let apiKey = 'your_api_key';
+let apiKey = '5b5f14723ccf3f576ce8c2e95b3b68bf9151303ea50c447f84ba9371';
 json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
   console.log(data.ip);
   console.log(data.city);
